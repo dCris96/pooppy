8a53e9cea46aec833aa715bd7d34275ed523cd11
Anthony-Crespin <dcris3344@gmail.com>
2023-11-03 16:56:08 -0500

    Finishied version movil

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-ScrollReveal().reveal('.title');
+ScrollReveal().reveal('.featured');
+ScrollReveal().reveal('.title__section');
+
+let btnTop = document.querySelector('.btn__top');
+
+window.onscroll = function() {
+    if (isScrolledIntoView(btnTop)) {
+        btnTop.style.display = "flex";
+    }
+};
+
+function isScrolledIntoView(btnTop) {
+    var rect = btnTop.getBoundingClientRect();
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
+
+    // Comprueba si el elemento está completamente visible en la ventana del navegador
+    return (elemTop >= 0) && (elemBottom <= window.innerHeight);
+}
